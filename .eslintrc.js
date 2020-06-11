05e17652dba6b418bffb9e69b8b9077363c67b76
한은이 <haneuneee@gmail.com>
2020-06-11 20:59:24 +0900

    Add prettier with eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,42 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es2020": true,
-      "jest": true,
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
   },
-  "extends": [
-      "plugin:react/recommended",
-      "airbnb",
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaFeatures": {
-        "jsx": true,
-      },
-      "ecmaVersion": 11,
-      "sourceType": "module",
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
   },
-  "plugins": [
-      "react",
-      "@typescript-eslint",
-  ],
-  "rules": {
-      "import/extensions": [
-        "error",
-        {
-          pattern: {
-            "js": "never",
-            "jsx": "never",
-            "ts": "never",
-            "tsx": "never",
-          },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "import/extensions": [
+      "error",
+      {
+        pattern: {
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
         },
-      ],
-      "react/jsx-filename-extension": ["error", { "extensions": [".jsx", ".tsx"] }],
-      "no-use-before-define": ["error", "nofunc"],
-      "no-param-reassign": ["error", { "props": true, "ignorePropertyModificationsForRegex": ["^registration$"] }]
+      },
+    ],
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "no-use-before-define": ["error", "nofunc"],
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsForRegex: ["^registration$"] },
+    ],
+    quotes: ["error", "double"],
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-        "typescript": {}
+      typescript: {},
     },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,9 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
+          {" "}
           <code>src/App.tsx</code>
-          {' '}
+          {" "}
           and save to reload.
         </p>
         <a
